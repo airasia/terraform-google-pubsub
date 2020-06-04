111141c61da875c334d3015147dd36927cf948fc
Syed Rakib Al Hasan <975768+syedrakib@users.noreply.github.com>
2020-06-04 12:58:26 +0800

    Add authentication support in PubSub push subscriptions

--- main.tf
+++ main.tf
@@ -14,6 +14,8 @@ locals {
     {
       name                       = format("%s-%s-push-%s", var.topic_name, subscription.name, var.name_suffix)
       push_endpoint              = subscription.push_endpoint
+      auth_sa_email              = lookup(subscription, "auth_sa_email", null)
+      auth_audience              = lookup(subscription, "auth_audience", null)
       ack_deadline_seconds       = lookup(subscription, "ack_deadline_seconds", var.default_ack_deadline_seconds)
       message_retention_duration = lookup(subscription, "message_retention_duration", var.default_message_retention_duration)
     }
@@ -46,6 +48,14 @@ resource "google_pubsub_subscription" "push_subscriptions" {
   message_retention_duration = local.push_subscriptions[count.index]["message_retention_duration"]
   push_config {
     push_endpoint = local.push_subscriptions[count.index]["push_endpoint"]
+    dynamic "oidc_token" {
+      # add this block only if 'auth_sa_email' is present for this subscription
+      for_each = local.push_subscriptions[count.index]["auth_sa_email"] == null ? [] : [1]
+      content {
+        service_account_email = local.push_subscriptions[count.index]["auth_sa_email"]
+        audience              = local.push_subscriptions[count.index]["auth_audience"]
+      }
+    }
   }
   depends_on = [google_project_service.pubsub_api]
 }
