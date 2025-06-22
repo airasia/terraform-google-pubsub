f51dffa496c1a5b2266834475ddc691e604e884c
snehar98 <85880102+snehar98@users.noreply.github.com>
2025-06-22 21:55:25 +0400

    Update main.tf
    Co-authored-by: Mohamed Fariz Nazeer <faariz@gmail.com>

--- main.tf
+++ main.tf
@@ -84,7 +84,8 @@ resource "google_pubsub_subscription" "push_subscriptions" {
   push_config {
     push_endpoint = local.push_subscriptions[count.index]["push_endpoint"]
     dynamic "no_wrapper" {
-      for_each = contains(keys(local.push_subscriptions[count.index]), "no_wrapper_write_metadata") ? [1] : []
+      # add this block only if 'no_wrapper_write_metadata' is present for this subscription
+      for_each = local.push_subscriptions[count.index]["no_wrapper_write_metadata"] == null ? [] : [1]
       content {
         write_metadata = local.push_subscriptions[count.index]["no_wrapper_write_metadata"]
       }
